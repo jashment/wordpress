95c1f1c7113eed3738221c5ae0f73262bab21c96
Jordan <jordanashment@gmail.com>
2020-03-09 21:42:44 -0600

    Fix styling in the read more for search

--- Construction_Site/construction_html/wp-content/themes/constructiontheme/functions.php
+++ Construction_Site/construction_html/wp-content/themes/constructiontheme/functions.php
@@ -5,6 +5,9 @@ add_filter('user can richedit', '__return_false', 50);
 // create menu item
 register_nav_menu('siteNavigation', 'Primary Navigation');
 
-
+function wpdocs_excerpt_more($more) {
+	return '<a href="'.get_the_permalink().'" rel="nofollow" id="readMore">Read More...</a>';
+}
+add_filter( 'excerpt_more', 'wpdocs_excerpt_more');
 
 ?>
